cd5aef76f7cbec7d951fa050e2cf41aaa3823396
Daniel Nyga <nyga@cs.uni-bremen.de>
2017-07-20 17:22:01 +0200

    added signals module

---
+++ dnutils/signals.py
@@ -0,0 +1,64 @@
+import signal as signal_
+import threading
+from collections import defaultdict
+
+handlers = defaultdict(list)
+
+SIGINT = signal_.SIGINT
+SIGTERM = signal_.SIGTERM
+SIGKILL = signal_.SIGKILL
+SIGABRT = signal_.SIGABRT
+SIGALRM = signal_.SIGALRM
+SIGBUS = signal_.SIGBUS
+SIGCHLD = signal_.SIGCHLD
+SIGCLD = signal_.SIGCLD
+SIGCONT = signal_.SIGCONT
+SIGFPE = signal_.SIGFPE
+SIGHUP = signal_.SIGHUP
+SIGILL = signal_.SIGILL
+SIGINT = signal_.SIGINT
+SIGIO = signal_.SIGIO
+SIGIOT = signal_.SIGIO
+SIGKILL = signal_.SIGKILL
+SIGPIPE = signal_.SIGPIPE
+SIGPOLL = signal_.SIGPOLL
+SIGPROF = signal_.SIGPROF
+SIGPWR = signal_.SIGPWR
+SIGQUIT = signal_.SIGQUIT
+SIGRTMAX = signal_.SIGRTMAX
+SIGRTMIN = signal_.SIGRTMIN
+SIGSEGV = signal_.SIGSEGV
+SIGSTOP = signal_.SIGSTOP
+SIGSYS = signal_.SIGSYS
+SIGTERM = signal_.SIGTERM
+SIGTRAP = signal_.SIGTRAP
+SIGTSTP = signal_.SIGTSTP
+SIGTTIN = signal_.SIGTTIN
+SIGTTOU = signal_.SIGTTOU
+SIGURG = signal_.SIGURG
+SIGUSR1 = signal_.SIGUSR1
+SIGUSR2 = signal_.SIGUSR2
+SIGVTALRM = signal_.SIGVTALRM
+SIGWINCH = signal_.SIGWINCH
+SIGXCPU = signal_.SIGXCPU
+SIGXFSZ = signal_.SIGXFSZ
+
+_lock = threading.Lock()
+
+def _run_handlers(signal, args):
+    for h in handlers[signal]:
+        h(*args)
+
+def add_handler(signal, handler):
+    with _lock:
+        handlers_ = handlers[signal]
+        if not handlers_:
+            signal_.signal(signal, lambda *args: _run_handlers(signal, args))
+        if handler not in handlers_:
+            handlers_.append(handler)
+
+def rm_handler(signal, handler):
+    with _lock:
+        try:
+            handlers[signal].remove(handler)
+        except ValueError: pass
