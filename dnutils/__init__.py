d99fcf4456f55d706e007d4b0d38d02c77e8491b
Daniel Nyga <nyga@cs.uni-bremen.de>
2017-07-20 17:23:29 +0200

    added imports

--- dnutils/__init__.py
+++ dnutils/__init__.py
@@ -7,3 +7,6 @@ from .debug import stop
 from .debug import trace
 from .debug import stoptrace
 from .tools import ifnone
+from .tools import ifnot
+from .signals import add_handler
+from .signals import rm_handler
