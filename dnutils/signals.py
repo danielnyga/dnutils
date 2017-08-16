d826e0fb6e7afa084ee38db71780838a39604cb6
Daniel Nyga <nyga@cs.uni-bremen.de>
2017-08-16 10:58:50 +0200

    use separate handler registries for system signal handlers and user-defined signal handlers

--- dnutils/signals.py
+++ dnutils/signals.py
@@ -2,7 +2,9 @@ import signal as signal_
 import threading
 from collections import defaultdict
 
-handlers = defaultdict(list)
+__usrreg = defaultdict(list)
+__sysinit = defaultdict(list)
+__systerm = defaultdict(list)
 
 SIGINT = signal_.SIGINT
 SIGTERM = signal_.SIGTERM
@@ -49,8 +51,15 @@ _lock = threading.Lock()
 def _run_handlers(signal, args):
     '''Executes all handlers that are registered for the given
     siganl in the order of registration.'''
-    for h in handlers[signal]:
-        h(*args)
+    # run system handlers for preparing the signal handling
+    for handler in __sysinit[signal]:
+        handler(*args)
+    # run user defined signal handlers
+    for handler in __usrreg[signal]:
+        handler(*args)
+    # run system handlers for cleaning up the signal handling
+    for handler in __systerm[signal]:
+        handler(*args)
 
 
 def add_handler(signal, handler):
@@ -61,12 +70,23 @@ def add_handler(signal, handler):
     :param handler: a callable that will be called on the signal.
     :return:
     '''
+    _add_handler(signal, handler, __usrreg)
+
+
+def _add_handler(signal, handler, registry):
+    '''
+    Add a handler to be executed on the signal ``signal``
+
+    :param signal:  the signal to react to.
+    :param handler: a callable that will be called on the signal.
+    :return:
+    '''
     with _lock:
-        handlers_ = handlers[signal]
+        handlers_ = registry[signal]
         if not handlers_:
             signal_.signal(signal, lambda *args: _run_handlers(signal, args))
         if handler not in handlers_:
-            handlers_.append(handler)
+            handlers_.insert(0, handler)
 
 
 def rm_handler(signal, handler):
@@ -76,10 +96,21 @@ def rm_handler(signal, handler):
     :param handler: the handler function.
     :return:
     '''
+    _rm_handler(signal, handler, __usrreg)
+
+
+def _rm_handler(signal, handler, registry):
+    '''
+    Remove a handler if it is registered to the given signal.
+    :param signal:  the signal that the handler is registered for
+    :param handler: the handler function.
+    :return:
+    '''
     with _lock:
         try:
-            handlers[signal].remove(handler)
-        except ValueError: pass
+            registry[signal].remove(handler)
+        except ValueError:
+            pass
 
 
 def keyint(*_):
@@ -91,7 +122,7 @@ def enable_ctrlc():
     Allows to interrupt the main thread by pressing Ctrl-C.
     :return:
     '''
-    add_handler(SIGINT, keyint)
+    _add_handler(SIGINT, keyint, __systerm)
 
 
 def disable_ctrlc():
@@ -99,4 +130,4 @@ def disable_ctrlc():
     Disables interruption of the main thread by the Ctrl-C key combination.
     :return:
     '''
-    rm_handler(SIGINT, keyint)
+    _rm_handler(SIGINT, keyint, __systerm)
