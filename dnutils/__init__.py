c8ad9f5f3e5d3276fbb08a8e6f68dea9b2160264
Daniel Nyga <nyga@cs.uni-bremen.de>
2017-08-28 16:15:14 +0200

    include exposure functions in __init__

--- dnutils/__init__.py
+++ dnutils/__init__.py
@@ -7,7 +7,7 @@ from .tools import ifnone, ifnot, allnone, allnot, edict, idxif, first, last
 from .signals import add_handler, rm_handler, enable_ctrlc
 from .threads import Lock, RLock, Condition, Event, Semaphore, BoundedSemaphore, Barrier, Relay, Thread, \
     SuspendableThread
-from .logs import loggers, newlogger, getlogger, DEBUG, INFO, WARNING, ERROR, CRITICAL
+from .logs import loggers, newlogger, getlogger, DEBUG, INFO, WARNING, ERROR, CRITICAL, expose, inspect, active_exposures
 from .console import ProgressBar, StatusMsg, bf
 
 enable_ctrlc()
