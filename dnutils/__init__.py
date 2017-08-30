d95a24a97f7f134fc98a3acfe9396126162d7420
Daniel Nyga <nyga@cs.uni-bremen.de>
2017-08-30 14:27:56 +0200

    fixed path

--- dnutils/__init__.py
+++ dnutils/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-with open(os.path.join(os.path.dirname(__file__), 'dnutils', 'VERSION'), 'r') as f:
+with open(os.path.join(os.path.dirname(__file__), 'VERSION'), 'r') as f:
     __version__ = f.read().strip()
 __author__ = 'Daniel Nyga'
 
