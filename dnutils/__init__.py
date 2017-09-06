2821b51b852b8f3200599262ff66ddb7578367b0
Daniel Nyga <nyga@cs.uni-bremen.de>
2017-09-06 12:04:49 +0200

    added imports

--- dnutils/__init__.py
+++ dnutils/__init__.py
@@ -6,7 +6,7 @@ from .debug import out, stop, trace, stoptrace
 from .tools import ifnone, ifnot, allnone, allnot, edict, idxif, first, last
 from .signals import add_handler, rm_handler, enable_ctrlc
 from .threads import Lock, RLock, Condition, Event, Semaphore, BoundedSemaphore, Barrier, Relay, Thread, \
-    SuspendableThread
+    SuspendableThread, sleep, waitabout
 from .logs import loggers, newlogger, getlogger, DEBUG, INFO, WARNING, ERROR, CRITICAL, expose, inspect, active_exposures
 from .console import ProgressBar, StatusMsg, bf
 
